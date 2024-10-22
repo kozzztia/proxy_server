763bcbb4c7c01a955e0f8d690cffbdaa10a86715
kozzztia <kozzztia@gmail.com>
2024-10-22 11:46:58 +0300

    set headers

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,9 @@
 export default async function handler(req, res) {
     const { page } = req.query;
     const apiKey = process.env.WALLHAVEN_API_KEY;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
     try {
         const response = await fetch(`https://wallhaven.cc/api/v1/search?apikey=${apiKey}&page=${page}`, {
