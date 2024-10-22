23d5f0920f6ed1d5ac530ade5e1d89c9b45135fa
kozzztia <kozzztia@gmail.com>
2024-10-22 12:18:10 +0300

    create const api

--- api/proxy.js
+++ api/proxy.js
@@ -1,12 +1,14 @@
 export default async function handler(req, res) {
     const { page } = req.query;
     const apiKey = process.env.WALLHAVEN_API_KEY;
+    const api = 'https://wallhaven.cc/api/v1/'
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
+    
     try {
-        const response = await fetch(`https://wallhaven.cc/api/v1/search?apikey=${apiKey}&page=${page}`, {
+        const response = await fetch(`${api}search?apikey=${apiKey}&page=${page}`, {
             headers: {
                 'Content-Type': 'application/json',
             }
