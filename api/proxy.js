0f654772394c4ffb3816a9192ed4f5b8ae559e56
kozzztia <kozzztia@gmail.com>
2024-10-24 08:54:34 +0300

    remove api to headers

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,18 @@
 export default async function handler(req, res) {
     const { page } = req.query;
     const apiKey = process.env.WALLHAVEN_API_KEY;
-    const api = 'https://wallhaven.cc/api/v1/'
+    const api = 'https://wallhaven.cc/api/v1/search'
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     
     try {
-        const response = await fetch(`${api}search?apikey=${apiKey}&page=${page}`, {
+        const response = await fetch(`${api}`, {
             headers: {
                 'Content-Type': 'application/json',
+                'Authorization': apiKey,
+                'page': page,
             }
         });
 
